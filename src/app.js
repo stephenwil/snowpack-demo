1d79183616eef4e1f93bee51efe2a2af65c52771
Stephen Wilson <stephenwil@gmail.com>
2020-02-18 22:58:58 +0000

    update

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 // Import your web-ready dependencies
-import { h, render } from '/web_modules/preact.js';
-import htm from '/web_modules/htm.js';
+import { h, render } from "/web_modules/preact.js";
+import htm from "/web_modules/htm.js";
+import get from "/web_modules/lodash.js";
+
+const fakeData = { sw: 1, x: { y: 1, z: { zz: 2 } } };
+const s = get(fakeData, "sw");
+console.log(s);
 const html = htm.bind(h);
 // Create your main app component
 function SomePreactComponent(props) {
-  return html`<h1 style="color: red">Hello, World!</h1>`;
+  return html`
+    <h1 style="color: red">Hello, World!</h1>
+  `;
 }
 // Inject your application into the an element with the id `app`.
-render(html`<${SomePreactComponent} />`, document.getElementById('app'));
+render(
+  html`
+    <${SomePreactComponent} />
+  `,
+  document.getElementById("app")
+);
